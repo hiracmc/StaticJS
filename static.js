263dda83f7c9eaf1c1fec0bdbc4f9764c6cb2aee
hiracmc <120374767+hiracmc@users.noreply.github.com>
2025-07-09 11:40:41 +0900

    v0

--- static.js
+++ static.js
@@ -1,1 +1,29 @@
 /* made by ponzu */
+
+async function pop(type,color,color2,text,textsize,sizex,sizey,customcss) {
+    
+    let { v1,v2,v3,v4 } = '';
+    switch (type) {
+        case 0 : v1 = `position: fixed;top: 0;right: 0;`; v2 = anim1; break;
+        case 1 : v1 = `position: fixed;bottom: 0;right: 0;`; v2 = anim1; break;
+        default: break;
+    }
+
+    const html = `
+        <p class="stjs_pop" style='
+        ${v1}
+        color: #${color};
+        background-color: #${color2};
+        width: ${sizex};
+        height: ${sizey};
+        font-size: ${textsize};
+        text-align: left;
+        animation: ${v2} 1.5s linear forwards;
+        ${customcss}
+        '>${text}</p>`
+    document.getElementById('stjs_pop').innerHTML = html;
+    const removepop = function a() {
+        document.getElementById('stjs_pop').innerHTML = '';
+    }
+    setTimeout(removepop,3000)
+}
